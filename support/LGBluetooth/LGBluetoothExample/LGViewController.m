2dd4e5cbf0c9d30a4443c1ffbb9f1df327566d3b
adrian santuario <santuario@ciencias.unam.mx>
2014-06-04 20:23:34 -0500

    bluetooth

--- support/LGBluetooth/LGBluetoothExample/LGViewController.m
+++ support/LGBluetooth/LGBluetoothExample/LGViewController.m
@@ -20,44 +20,93 @@
      {
          // If we found any peripherals sending to test
          if (peripherals.count) {
-             [self testPeripheral:peripherals[0]];
+             NSLog(@"peripherals.count :: %@", [@(peripherals.count) stringValue] );
+             //[self testPeripheral:peripherals[0]];
+             [self connectToPanicButtonDevice:peripherals[0]];
          }
+         
+         
+         
+         [LGUtils readDataFromCharactUUID:@"ffe1"
+                              serviceUUID:@"ffe0"
+                               peripheral:peripherals[0]
+                               completion:^(NSData *data, NSError *error) {
+                                   NSLog(@"Data : %s Error : %@", (char *)[data bytes], error);
+                               }];
+         
+         
      }];
 }
 
 - (void)testPeripheral:(LGPeripheral *)peripheral
 {
-    // First of all connecting to peripheral
+
+
+    // First of all, opening connection
     [peripheral connectWithCompletion:^(NSError *error) {
         // Discovering services of peripheral
         [peripheral discoverServicesWithCompletion:^(NSArray *services, NSError *error) {
+            // Searching in all services, our - 5ec0 service
             for (LGService *service in services) {
-                // Finding out our service
-                NSLog(@" >>>>>>>>  service.UUIDString: %@", service.UUIDString);
-                if ([service.UUIDString isEqualToString:@"ffe0"]) {
-                    // Discovering characteristics of our service
+                if ([service.UUIDString isEqualToString:@"5ec0"]) {
+                    // Discovering characteristics of 5ec0 service
                     [service discoverCharacteristicsWithCompletion:^(NSArray *characteristics, NSError *error) {
-                        // We need to count down completed operations for disconnecting
                         __block int i = 0;
+                        // Searching writable characteristic - cef9
                         for (LGCharacteristic *charact in characteristics) {
-                            
-                            NSLog(@" >>>>>>>>  charact: %@", charact.UUIDString);
-
-                            // cef9 is a writabble characteristic, lets test writting
-                            if ([charact.UUIDString isEqualToString:@"ffe1"]) {
-                                [charact readValueWithBlock:^(NSData *data, NSError *error) {
+                            if ([charact.UUIDString isEqualToString:@"cef9"]) {
+                                [charact writeByte:0xFF completion:^(NSError *error) {
                                     if (++i == 3) {
-                                        
-                                        NSLog(@":-:-:-: READ :-:-:-: ");
-
-                                        // finnally disconnecting
-                                       // [peripheral disconnectWithCompletion:nil];
+                                        [peripheral disconnectWithCompletion:nil];
                                     }
                                 }];
-
-                            }                         }
+                            } else {
+                                // Otherwise reading value
+                                [charact readValueWithBlock:^(NSData *data, NSError *error) {
+                                    if (++i == 3) {
+                                        [peripheral disconnectWithCompletion:nil];
+                                    }
+                                }];
+                            }
+                        }
                     }];
                 }
+            }
+        }];
+    }];
+    
+    
+}
+
+
+-(void)connectToPanicButtonDevice:(LGPeripheral *)peripheral{
+    
+    [peripheral connectWithCompletion:^(NSError *error) {
+        // Discovering services of peripheral
+        [peripheral discoverServicesWithCompletion:^(NSArray *services, NSError *error) {
+            // Searching in all services, our - 5ec0 service
+            for (LGService *service in services) {
+                
+                NSLog(@"Service :: %@ UUID :: %@", service.description, service.UUIDString);
+                
+                
+                
+                if ([service.UUIDString isEqualToString:@"ffe0"]) {
+                    // Discovering characteristics of 5ec0 service
+                    [service discoverCharacteristicsWithCompletion:^(NSArray *characteristics, NSError *error) {
+                       
+                        // Searching writable characteristic
+                        for (LGCharacteristic *charact in characteristics) {
+       
+                            NSLog(@"characteristics :: %@" , charact.UUIDString);
+                            
+                            
+                        }
+                    }];
+                }
+                
+                
+                
             }
         }];
     }];
@@ -69,5 +118,18 @@
     // Initialization of CentralManager
     [LGCentralManager sharedInstance];
 }
+
+
+
+
+/*
+ffe0
+1803 Link Loss
+1802 Immediate Alert
+1804 Tx Power
+180f Battery Service
+180a Device Information
+*/
+
 
 @end
